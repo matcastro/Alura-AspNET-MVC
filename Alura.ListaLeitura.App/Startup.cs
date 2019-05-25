9dafa0375164520bcf16fbc50014cfa1b27622ea
Mateus Castro <matcastro>
2019-05-25 18:27:49 -0300

    Transformando a aplicação console em uma aplicação web e criando um serviço rudimentar de roteamento.

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(Roteamento);
+        }
+
+        public Task Roteamento(HttpContext contexto)
+        {
+            var rotas = new Dictionary<string, RequestDelegate>()
+            {
+                { "/Livros/ParaLer", LivrosParaLer },
+                { "/Livros/Lidos", LivrosLidos },
+                { "/Livros/Lendo", LivrosLendo }
+            };
+            if (rotas.ContainsKey(contexto.Request.Path))
+            {
+                return rotas[contexto.Request.Path].Invoke(contexto);
+            }
+            contexto.Response.StatusCode = 404;
+            return contexto.Response.WriteAsync("Caminho inexistente");
+        }
+
+        public Task LivrosParaLer(HttpContext contexto)
+        {
+            var repositorio = new LivroRepositorioCSV();
+            return contexto.Response.WriteAsync(repositorio.ParaLer.ToString());
+        }
+        public Task LivrosLidos(HttpContext contexto)
+        {
+            var repositorio = new LivroRepositorioCSV();
+            return contexto.Response.WriteAsync(repositorio.Lidos.ToString());
+        }
+        public Task LivrosLendo(HttpContext contexto)
+        {
+            var repositorio = new LivroRepositorioCSV();
+            return contexto.Response.WriteAsync(repositorio.Lendo.ToString());
+        }
+    }
+}
